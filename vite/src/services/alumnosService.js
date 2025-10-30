b5d2f3b0fb3cd326e9ef54948a9796134868f77d
1608-EQUIPOSISTEMAS <sistemasbd.we@gmail.com>
2025-10-29 19:34:37 -0500

    changed: cambio de ruta

--- vite/src/services/alumnosService.js
+++ vite/src/services/alumnosService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { authService } from './authService';
 
-const API_URL = 'http://localhost:3005/api';
+const API_URL = 'https://sistemabackendacademica.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
--- vite/src/services/authService.js
+++ vite/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3005/api/auth';
+const API_URL = 'https://sistemabackendacademica.onrender.com/api/auth';
 
 export const authService = {
   // Login y guardar token
